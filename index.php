3aa4cb0fd73fb28cf351673675da50a48ac9bd7e
JosteinO <josteinmeg@gmail.com>
2018-04-23 16:17:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
+             "Portnumber: ".$_SERVER['SERVER_PORT'];
+        ?>
+    </body>
+</html>
