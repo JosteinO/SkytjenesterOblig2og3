84dbb6e242cf217df172cc809e1b9bdedae3a6c4
Magnus Hansen-Møllerud <s305505@stud.hioa.no>
2018-04-27 13:46:03 +0200

    Update retrieveInfo.php

--- retrieveInfo.php
+++ retrieveInfo.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
- <head>
-   <title>Retrieve Info</title>
- </head>
-   <body>
-    <form action="" method="post">
-      <input type="submit" name="retrieve" value="Retrieve all the data!"></input><br>
-    </form>
-     <?php
-        echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
-       "Portnumber: ".$_SERVER['SERVER_PORT']."<br>";
-     ?>
-    <footer>
-    </footer>
  <?php
           $db = mysqli_connect("10.10.2.5", "maxscaleuser", "maxscalepass", "studentinfo");
           $info = array();
@@ -43,6 +28,21 @@
                 }
             }
 ?>
+<!DOCTYPE html>
+<html>
+ <head>
+   <title>Retrieve Info</title>
+ </head>
+   <body>
+    <form action="" method="post">
+      <input type="submit" name="retrieve" value="Retrieve all the data!"></input><br>
+    </form>
+    <footer>
+     <?php
+        echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
+       "Portnumber: ".$_SERVER['SERVER_PORT']."<br>";
+     ?>
+    </footer>
   </body>
 </html>
 
