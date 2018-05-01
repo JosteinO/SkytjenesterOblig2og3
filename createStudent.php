6f9224068f557523bb0c5fe3769701a3ebabf6cb
Magnus Hansen-Møllerud <s305505@stud.hioa.no>
2018-05-01 18:10:13 +0200

    Update createStudent.php

--- createStudent.php
+++ createStudent.php
@@ -21,7 +21,7 @@ $studyprog = $_REQUEST["studyprogram"];
 echo "$studentID <br>" . "$name <br>" . " $email <br>" . "$studyprog";
   
 $sql = "INSERT INTO studentinfo.students (studentid, name, email, courseID) VALUES"
-            . "('$studentID', '$name', '$email')";
+            . "('$studentID', '$name', '$email', '')";
     
 $sql2 = "INSERT INTO studentinfo.courses (study_program) VALUES"
             . "('$studyprog')";
