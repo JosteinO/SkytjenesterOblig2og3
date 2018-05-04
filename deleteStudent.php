996b98e2b0f68ce9b5673b48fddc5d6e073f1ac9
JosteinO <josteinmeg@gmail.com>
2018-05-04 10:09:47 +0200

    Style deleteStudent.php

--- deleteStudent.php
+++ deleteStudent.php
@@ -4,62 +4,67 @@
    <meta charset="utf-8">
    <title>Delete student</title>
  </head>
+ <div id="container">
    <body>
+     <div id="header">
+       <h2>Delete a student</h2>
+     </div>
+     <div id="body">
       <form action="" method="post">
-          Type in studentid for the student you want to delete: <input type="text" name="studentid" placeholder="sXXXXXX"></input>
-       <input type="submit" value="Delete student" name="delete"></input>
-      <form action="" method="post">
+        Type in studentid for the student you want to delete: <input type="text" name="studentid" placeholder="sXXXXXX"></input>
+        <input type="submit" value="Delete student" name="delete"></input>
+        <form action="" method="post">
           <input type="submit" value="Back to navigation-page" name="toIndex"></input>
+        </form>
       </form>
-      </form>
 
-<?php
-                
-          if(isset($_POST["delete"])){
-              $servername = "10.10.2.5";
-              $username = "dats20";
-              $password = "passord";
-              $dbname = "studentinfo";
-              // Create connection
-              $db = new mysqli($servername, $username, $password, $dbname);
-              // Check connection
-              if ($db->connect_error) {
-                  die("Connection failed: " . $db->connect_error);
-              }
-           
-              $studentid = $_POST["studentid"];
-              
-              /*$sql = "DELETE s, c
-              FROM students s
-              JOIN courses c ON s.'$studentid'=c.'$studentid'
-              WHERE s.studentid='$studentid' AND c.studentid='$studentid'";
-              $resultat = $db->query($sql);*/
-           
-              $sql = "DELETE
-              FROM courses
-              WHERE studentid='$studentid'";
-           
-              $sql2 = "DELETE
-              FROM students
-              WHERE studentid='$studentid'";
-           
-              $resultat = $db->query($sql);   
-              $resultat2 = $db->query($sql2);
-           
-              echo $studentid . " is now deleted!";
-              $db->close();
-          }
+      <?php
 
-          if(isset($_POST["toIndex"])){
-           header("Location: index.php");
-          }
-?>
+      if(isset($_POST["delete"])){
+        $servername = "10.10.2.5";
+        $username = "dats20";
+        $password = "passord";
+        $dbname = "studentinfo";
+        // Create connection
+        $db = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($db->connect_error) {
+          die("Connection failed: " . $db->connect_error);
+        }
 
-    <footer style="border-style: solid; text-align: center;">
+        $studentid = $_POST["studentid"];
+
+        /*$sql = "DELETE s, c
+        FROM students s
+        JOIN courses c ON s.'$studentid'=c.'$studentid'
+        WHERE s.studentid='$studentid' AND c.studentid='$studentid'";
+        $resultat = $db->query($sql);*/
+
+        $sql = "DELETE
+        FROM courses
+        WHERE studentid='$studentid'";
+
+        $sql2 = "DELETE
+        FROM students
+        WHERE studentid='$studentid'";
+
+        $resultat = $db->query($sql);
+        $resultat2 = $db->query($sql2);
+
+        echo $studentid . " is now deleted!";
+        $db->close();
+      }
+
+      if(isset($_POST["toIndex"])){
+        header("Location: index.php");
+      }
+      ?>
+  </div>
+  <div id="footer">
      <?php
-       echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
-       "Portnumber: ".$_SERVER['SERVER_PORT']."<br>";
+       include "Footer.php";
      ?>
-    </footer>
+  </div>
   </body>
+</div>
 </html>
