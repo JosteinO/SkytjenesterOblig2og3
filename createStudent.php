567304f3201fcf5cced30e2a99e2bad0e58759b6
JosteinO <josteinmeg@gmail.com>
2018-05-04 09:54:08 +0200

    Style createStudent.php

--- createStudent.php
+++ createStudent.php
@@ -47,31 +47,35 @@ $sql2 = "INSERT INTO studentinfo.courses (study_program, studentid) VALUES"
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Create student</title>
   </head>
+  <div id id="container">
   <body>
+    <div id="header">
       <h2>Create student</h2>
       <hr><br>
- <form action="" method="POST">
-  <fieldset>
-    <legend>Student information:</legend>
-    StudentID:<br>
-    <input type="text" name="studentid" placeholder="Enter StudentID"><br>
-    Name:<br>
-    <input type="text" name="name" placeholder="Enter your name"><br>
-    Email:<br>
-    <input type="text" name="email" placeholder="Enter your email"><br>
-    Study program:<br>
-    <input type="text" name="studyprogram" placeholder="Enter your study program"><br><br>
-    <input type="submit" name="createStudent" value="Submit">
-  </fieldset>
-</form>
+    </div>
+    <div id="body">
+      <form action="" method="POST">
+        <fieldset>
+          <legend>Student information:</legend>
+          StudentID:<br>
+          <input type="text" name="studentid" placeholder="Enter StudentID"><br>
+          Name:<br>
+          <input type="text" name="name" placeholder="Enter your name"><br>
+          Email:<br>
+          <input type="text" name="email" placeholder="Enter your email"><br>
+          Study program:<br>
+          <input type="text" name="studyprogram" placeholder="Enter your study program"><br><br>
+          <input type="submit" name="createStudent" value="Submit">
+        </fieldset>
+      </form>
 
-<br><br>Go back: <a href="/index.php">Click here</a><br><br><hr>
+      <br><br>Go back: <a href="/index.php">Click here</a><br><br><hr>
 
-      <footer>
+  </div>
+  <div id="footer">
       <?php
-        echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
-             "Portnumber: ".$_SERVER['SERVER_PORT'];
+        include "Footer.php";
         ?>
-      </footer>
+  </div>
   </body>
 </html>
