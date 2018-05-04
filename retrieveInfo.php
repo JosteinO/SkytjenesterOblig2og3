9c94047a5d963330aa605bf1c11a663c905f0cb9
JosteinO <josteinmeg@gmail.com>
2018-05-04 09:41:18 +0200

    Style på retrieveinfo.php

--- retrieveInfo.php
+++ retrieveInfo.php
@@ -4,61 +4,66 @@
    <meta charset="utf-8">
    <title>Retrieve Info</title>
  </head>
+ <div id="container">
+
    <body>
+     <div id="header">
+       This is the students saved in the db cluster
+     </div>
+     <div id="body">
       <form action="" method="post">
        <input type="submit" value="Back to navigation-page" name="toIndex"></input>
       </form>
 
-<?php
+      <?php
 
-                $servername = "10.10.2.5";
-                $username = "dats20";
-                $password = "passord";
-                $dbname = "studentinfo";
+        $servername = "10.10.2.5";
+        $username = "dats20";
+        $password = "passord";
+        $dbname = "studentinfo";
 
-                // Create connection
-                $db = new mysqli($servername, $username, $password, $dbname);
+        // Create connection
+        $db = new mysqli($servername, $username, $password, $dbname);
 
-                // Check connection
-                if ($db->connect_error) {
-                    die("Connection failed: " . $db->connect_error);
-                }
+        // Check connection
+        if ($db->connect_error) {
+          die("Connection failed: " . $db->connect_error);
+        }
 
-                 $sql = "SELECT s.studentid, s.name, s.email, c.study_program
-                 FROM students s
-                 LEFT JOIN courses c ON s.studentid = c.studentid";
+        $sql = "SELECT s.studentid, s.name, s.email, c.study_program
+        FROM students s
+        LEFT JOIN courses c ON s.studentid = c.studentid";
 
-                 $resultat = $db->query($sql);
+        $resultat = $db->query($sql);
 
-                 $antall_rader = $db->affected_rows;
+        $antall_rader = $db->affected_rows;
 
 
-                 for ($i = 0; $i < $antall_rader; $i++) {
-                     $rad = $resultat->fetch_object();
-                     if($rad > 0){
-                      $studentid = "StudentID: " . $rad->studentid;
-                      $name = "Name: " . $rad->name;
-                      $email = "Email: " . $rad->email;
-                      $studyprogram = "Studyprogram: " . $rad->study_program;
-                      echo $studentid . "<br>";
-                      echo $name . "<br>";
-                      echo $email . "<br>";
-                      echo $studyprogram . "<br><br>";
-                     }
-                 }
-
-                 $db->close();
-
-          if(isset($_POST["toIndex"])){
-           header("Location: index.php");
+        for ($i = 0; $i < $antall_rader; $i++) {
+          $rad = $resultat->fetch_object();
+          if($rad > 0){
+            $studentid = "StudentID: " . $rad->studentid;
+            $name = "Name: " . $rad->name;
+            $email = "Email: " . $rad->email;
+            $studyprogram = "Studyprogram: " . $rad->study_program;
+            echo $studentid . "<br>";
+            echo $name . "<br>";
+            echo $email . "<br>";
+            echo $studyprogram . "<br><br>";
           }
-?>
+        }
 
-    <footer style="border-style: solid; text-align: center;">
-     <?php
-        echo "Server IP: ".$_SERVER['SERVER_ADDR']." <br/> ".
-       "Portnumber: ".$_SERVER['SERVER_PORT']."<br>";
-     ?>
-    </footer>
+        $db->close();
+
+        if(isset($_POST["toIndex"])){
+          header("Location: index.php");
+        }
+        ?>
+        </div>
+        <div id="footer">
+          <?php
+          include "Footer.php";
+          ?>
+        </div>
   </body>
 </html>
